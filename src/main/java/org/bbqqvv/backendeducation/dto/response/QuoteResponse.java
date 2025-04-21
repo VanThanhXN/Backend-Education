12e17db030f4034750dfe9911052940eeb6ca603
VanThanhXN <thanhlearning03@gmail.com>
2025-04-21 12:39:16 +0700

    ddff

--- src/main/java/org/bbqqvv/backendeducation/dto/response/QuoteResponse.java
+++ src/main/java/org/bbqqvv/backendeducation/dto/response/QuoteResponse.java
@@ -5,12 +5,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class QuoteResponse {
-    private String id;
-    private String content;
-    private String author;
-}
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public class QuoteResponse {
+        private String id;
+        private String content;
+        private String author;
+    }
